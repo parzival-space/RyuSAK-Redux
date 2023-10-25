1c7101c8f1ae0420f04e1a8830a61022b9d93a6b
parzival <me@parzival.space>
2023-10-25 16:54:35 +0200

    fix icon positions

--- client/src/renderer.ts
+++ client/src/renderer.ts
@@ -4,15 +4,15 @@ import App from "./App.vue";
 // Roboto Font
 import '@fontsource/roboto';
 
+// Font Awesome 5
+import '@fortawesome/fontawesome-free/css/all.css' // must be loaded beforce vuetify for now
+import { aliases, fa } from 'vuetify/iconsets/fa'
+
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-
-// Font Awesome 5
-import '@fortawesome/fontawesome-free/css/all.css' // Ensure your project is capable of handling css files
-import { aliases, fa } from 'vuetify/iconsets/fa'
 
 // Setup Vuetify
 const vuetify = createVuetify({
